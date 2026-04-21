61aa9e88f04eb02580869be25a07b3391ad44e49
JustinGohLab <gohjustin100@gmail.com>
2026-04-22 02:12:04 +0800

    a

--- js/douban.js
+++ js/douban.js
@@ -526,6 +526,7 @@ function renderDoubanCards(data, container) {
             // 处理图片URL
             // 1. 直接使用豆瓣图片URL (添加no-referrer属性)
             const originalCoverUrl = item.cover;
+            console.log('aa:', originalCoverUrl);
             
             // 2. 也准备代理URL作为备选
             const proxiedCoverUrl = PROXY_URL + encodeURIComponent(originalCoverUrl);
@@ -533,7 +534,6 @@ function renderDoubanCards(data, container) {
             // 为不同设备优化卡片布局
             card.innerHTML = `
                 <div class="relative w-full aspect-[2/3] overflow-hidden cursor-pointer" onclick="fillAndSearchWithDouban('${safeTitle}')">
-                    <source srcset="${originalCoverUrl}" type="image/webp">
                     <img src="${originalCoverUrl}" alt="${safeTitle}" 
                         class="w-full h-full object-cover transition-transform duration-500 hover:scale-110"
                         onerror="this.onerror=null; this.src='${proxiedCoverUrl}'; this.classList.add('object-contain');"
